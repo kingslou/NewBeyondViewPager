c99b867e25a08a9d51fc0c0168bc11c5c47fcd71
firesoul <690812619@qq.com>
2015-05-13 18:13:43 +0800

    initiate CardView

---
+++ sample/src/main/java/com/toaker/newbeyondviewpager/demo/CardView.java
@@ -0,0 +1,50 @@
+package com.toaker.newbeyondviewpager.demo;
+
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.view.View;
+/**
+ * Created by luchao on 2015/5/13.
+ */
+
+
+public class CardView extends View
+{
+    private Bitmap[] mCards = new Bitmap[3];
+
+    private int[] mImgId = new int[]{};
+
+    public CardView(Context context)
+    {
+        super(context);
+
+        Bitmap bm = null;
+        for (int i = 0; i < mCards.length; i++)
+        {
+            bm = BitmapFactory.decodeResource(getResources(), mImgId[i]);
+            mCards[i] = Bitmap.createScaledBitmap(bm, 400, 600, false);
+        }
+
+        setBackgroundColor(0xff00ff00);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas)
+    {
+
+        super.onDraw(canvas);
+
+        canvas.save();
+        canvas.translate(20, 120);
+        for (Bitmap bitmap : mCards)
+        {
+            canvas.translate(120, 0);
+            canvas.drawBitmap(bitmap, 0, 0, null);
+        }
+        canvas.restore();
+
+    }
+}
